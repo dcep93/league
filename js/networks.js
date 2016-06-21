314ddeb6a78b687104b0fa980c5d9b06763f60ed
Daniel Cepeda <dcep93@gmail.com>
2016-06-21 12:08:19 -0700

    define network dict - getJSON still broken

--- js/networks.js
+++ js/networks.js
@@ -1,9 +1,9 @@
-var networks;
+var networks = {};
 
 for(var game in games){
 	networks[game] = {};
 	for(var division in games[game].divisions){
-		$.getJSON('../networks/'+game+'/'+division, function(json){
+		$.getJSON('../networks/'+game+'/'+division+'.json', function(json){
 			networks[json.game][json.division] = json;
 		});
 	}
