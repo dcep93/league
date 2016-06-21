4122e48f30119e329a95afb27c1d93dc8e32c6a3
Daniel Cepeda <dcep93@gmail.com>
2016-06-21 11:12:22 -0700

    games generality single object

--- js/design.js
+++ js/design.js
@@ -1,28 +1,30 @@
 var game;
 
 $(document).ready(function(){
-	game = league;
+	game = 'league';
 	initialize();
 });
 
 function initialize(){
 	$('.picks').empty();
 
-	for(var i=0;i<game.teamSize;i++){
+	var numPicks = games[game].order.length / 2;
+
+	for(var i=0;i<numPicks;i++){
 		$('<div class="pick">').appendTo('.picks');
 	}
 
 	$("#champ-pool").empty().append($('<img class="champ">').attr("src", "images/shared/undo.png").click(undo));
 	$('.bans').empty();
 
-	for(var champ of game.champs){
+	for(var champ of games[game].champs){
 		$('#champ-pool').append(buildSquare(champ));
 		$('.bans').append(buildBan(champ));
 	}
 
 	$('#division').empty();
-	for(var d in game.divisions){
-		$("<option>").attr('value', d).text(game.divisions[d]).appendTo("#division");
+	for(var d in games[game].divisions){
+		$("<option>").attr('value', d).text(games[game].divisions[d]).appendTo("#division");
 	}
 	
 
@@ -48,7 +50,7 @@ function initialize(){
 }
 
 function buildSquare(champ){
-	return $('<img class="champ">').attr("src", "images/" + game.name + "/square/" + champ + ".png").attr('data-champ', champ).click(pick);
+	return $('<img class="champ">').attr("src", "images/" + game + "/square/" + champ + ".png").attr('data-champ', champ).click(pick);
 }
 
 function buildBan(champ){
@@ -56,18 +58,18 @@ function buildBan(champ){
 }
 
 function buildSplash(champ){
-	return $('<img class="splash">').attr('src', "images/" + game.name + "/splash/" + champ + ".jpg").attr('data-champ', champ);
+	return $('<img class="splash">').attr('src', "images/" + game + "/splash/" + champ + ".jpg").attr('data-champ', champ);
 }
 
 function getPick(index){
-	var team = game.order[index];
-	var n = game.order.slice(0,index).reduce(function(n, t) { return n + (t === team) }, 0);
+	var team = games[game].order[index];
+	var n = games[game].order.slice(0,index).reduce(function(n, t) { return n + (t === team) }, 0);
 	return $("#" + team + "-team .pick").eq(n);
 }
 
 function pick(){
 	var splashes = $('.splash');
-	if(game.order.length > splashes.length){
+	if(games[game].order.length > splashes.length){
 		var champ = $(this).attr('data-champ');
 		if(splashes.filter('[data-champ="' + champ + '"]').length == 0){
 			getPick(splashes.length).html(buildSplash(champ));
@@ -83,7 +85,7 @@ function undo(){
 }
 
 function getTeam(team){
-	return $(team).find('.splash').map(function(){ return game.champs.indexOf($(this).attr('data-champ')) }).toArray();
+	return $(team).find('.splash').map(function(){ return games[game].champs.indexOf($(this).attr('data-champ')) }).toArray();
 }
 
 function start(){
@@ -91,7 +93,7 @@ function start(){
 	$("#start").attr("disabled",'');
 
 	data = {
-		'game': game.name,
+		'game': game,
 		'blueTeam': getTeam('#blue-team'),
 		'redTeam': getTeam('#red-team'),
 		'division': $('#division').val(),
