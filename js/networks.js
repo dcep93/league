091bcc36b5102146d659e7b10bab0c9960413dd0
Daniel Cepeda <dcep93@gmail.com>
2016-06-21 12:32:18 -0700

    networks within league

--- js/networks.js
+++ js/networks.js
@@ -3,7 +3,7 @@ var networks = {};
 for(var game in games){
 	networks[game] = {};
 	for(var division in games[game].divisions){
-		$.getJSON('../networks/'+game+'/'+division+'.json', function(json){
+		$.getJSON('networks/'+game+'/'+division+'.json', function(json){
 			networks[json.game][json.division] = json;
 		});
 	}
